39f0ee56ade7cb31929abc9b6d8072006996b3e0
STEVE OVSAK <steveovsak@MacBook-Pro.local>
2021-01-08 16:37:57 -0700

    coding

--- Instructions/StarterCode/assets/js/app.js
+++ Instructions/StarterCode/assets/js/app.js
@@ -9,14 +9,42 @@ var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper and append SVG group to the chart
-var svg = d3
-    .select(".chart")
-    .append("svg")
+var svg = d3.select("#scatter").append("svg")
     .attr("width", svgWidth)
     .attr("height", svgHeight)
-    .append("g")
-    .attr("transform", "translate(" +margin.left +"," +margin.top + ")");
 
-// Append SVG group
-var chart = svg.append("g");
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
+
+// Append div to body, create tooltips and assign class
+d3.sleect(".chart").append("div").attr("class", "tooltip").style("opacity", 0);
+
+// CSV retreival and execution
+d3.csv("data.csv").then(funciton(censusData)) {
+    
+    console.log(censusData)
+
+    censusData.forEach(function(data) {
+        data.healthcare = +data.heathcare;
+        data.poverty = +data.poverty;
+    });
+
+//Create scale functions
+    //==================
+    
+    var ylinearScale = d3.scaleLinear()
+    .domain([d3.min(censusData, d => d.heathcare) -2, d3.max(censusData, d => d.heathcare) +2])
+    .range([chartHeight, 0]);
+
+    var xlinearScale = d3.scaleLinear()
+    .domain([d3.min(censusData, d => d.poverty) -1, d3.max(censusData, d => d.poverty) +1])
+    .range([0, chartwidth]);
+
+// Axis functions
+    //====================
+
+    var yAxis = d3.axisLeft(ylinearScale);
+    var xAxis = d3.axisBottom(xlinearScale);
+    
+})
