572e4aca26b8b29fbe11fd820765be9eef0c0865
STEVE OVSAK <steveovsak@MacBook-Pro.local>
2021-01-08 16:12:16 -0700

    Started Code

--- Instructions/StarterCode/assets/js/app.js
+++ Instructions/StarterCode/assets/js/app.js
@@ -1,1 +1,22 @@
 // @TODO: YOUR CODE HERE!
+
+var svgWidth = 900;
+var svgHeight = 500;
+
+var margin = { top:20, right: 80, left: 100 };
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper and append SVG group to the chart
+var svg = d3
+    .select(".chart")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight)
+    .append("g")
+    .attr("transform", "translate(" +margin.left +"," +margin.top + ")");
+
+// Append SVG group
+var chart = svg.append("g");
+
